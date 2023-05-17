88f639c013d8c07e3b0a4a2c57ed67a39f65b70e
Bailey Yates Armitage <yeliab@desktop-91iq4bl.uab.cat>
2023-05-17 16:12:20 +0200

    Started to build models, added some more util functions

--- models/models.py
+++ models/models.py
@@ -1,6 +1,33 @@
+from turtle import forward
 import torch.nn as nn
+import torch
 
-# Conventional and convolutional neural network
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+class EncoderRNN(nn.Module):
+    def __init__(self, input_size, hidden_size):
+        super(EncoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(input_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+
+    def forward(self, input, hidden):
+        embedded = self.embedding(input).view(1, 1, -1)
+        output = embedded
+        output, hidden = self.gru(output, hidden)
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
+
+class DecoderRNN(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+
+# Conventional and convolutional neural network - This is the old nn
 
 class ConvNet(nn.Module):
     def __init__(self, kernels, classes=10):
